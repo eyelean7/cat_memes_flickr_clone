904722f622f0d8f1552f0bc2aa5a43169a127925
Ilene Gorski and Katie Borel <ilene+katie@>
2017-07-05 11:50:03 -0700

    add devise gem functionality and build homepage

---
+++ db/migrate/20170705182759_images_table.rb
@@ -0,0 +1,9 @@
+# class ImagesTable < ActiveRecord::Migration[5.1]
+#   def change
+#     create_table :images do |t|
+#       t.string :keywords
+#       t.string :email
+#       t.timestamps
+#     end
+#   end
+# end
